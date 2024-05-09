b870c15502592dfabbc7047a09d6e34ecd088ce6
KAKI555 <158486761+KAKI555@users.noreply.github.com>
2024-05-09 19:51:18 +0530

    Update main_84.js

--- main_84.js
+++ main_84.js
@@ -1,13 +1,20 @@
-pizza_list_array = [];
+menu_list_array = [ "Veg Margherita Pizza","Sicilian Pizza","Pizza Fritta"]; //Add more Pizza item names
 
 function getmenu()
-	{    
-        // Give the appropriate id name as pizza_name
-        var item = document.getElementById("        ").value; 
-       
-        //Add push() function to push item into array
-        pizza_list_array.      (item);
-        
-        var remove_commas = pizza_list_array.join("<br># ");
-        document.getElementById("display_menu").innerHTML =  "# " + remove_commas;
-	}
+{
+    var htmldata="";
+    for(var i=0; i<menu_list_array.length; i++)
+    {
+        htmldata=htmldata+ menu_list_array[i] + '<br>'
+    }
+    //Give the appropriate id name as display_menu 
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_suggestion(){
+    //Give the appropriate id name as add_item
+	var item = document.getElementById("add_item").value;
+    //Use the push() function to push the item into menu_list_array
+    menu_list_array.push(item);
+}
+
